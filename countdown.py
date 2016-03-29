2a5f8bb69e74cb0850590da526723180a0714553
brionna17 <brionna.ferrell@ms223.org>
2016-03-29 09:00:30 -0400

    Create countdown.py

---
+++ countdown.py
@@ -0,0 +1,11 @@
+# number = input("Pick a number?")
+# for x in range(1,number + 1):
+#     print number
+#     number = number - 1
+# print "boom!"
+
+x = input("pick a number?")
+while x > 0:
+    print x
+    x = x - 1
+print "boom!"
